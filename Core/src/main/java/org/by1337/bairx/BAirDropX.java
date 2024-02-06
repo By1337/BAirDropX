2145ef45f845e190c38661bdf484b5d4fc133a69
By1337 <by133700@gmail.com>
2024-02-06 18:37:48 +0300

    Initial commit

---
+++ Core/src/main/java/org/by1337/bairx/BAirDropX.java
@@ -0,0 +1,17 @@
+package org.by1337.bairx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BAirDropX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
