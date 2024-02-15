6ccefc633123178e2fc8c8316373613c9b569993
By1337 <by133700@gmail.com>
2024-02-15 16:57:17 +0300

    feat: nbt

---
+++ Core/src/main/java/org/by1337/bairx/nbt/NBT.java
@@ -0,0 +1,37 @@
+package org.by1337.bairx.nbt;
+
+public abstract class NBT {
+    public abstract String toString();
+    public abstract NbtType getType();
+    public abstract String toStringBeautifier(int lvl);
+    public abstract Object getAsObject();
+    public String quoteAndEscape(String raw) {
+        StringBuilder var1 = new StringBuilder(" ");
+        int var2 = 0;
+
+        for (int var3 = 0; var3 < raw.length(); ++var3) {
+            char var4 = raw.charAt(var3);
+            if (var4 == '\\') {
+                var1.append('\\');
+            } else if (var4 == '"' || var4 == '\'') {
+                if (var2 == 0) {
+                    var2 = var4 == '"' ? 39 : 34;
+                }
+
+                if (var2 == var4) {
+                    var1.append('\\');
+                }
+            }
+
+            var1.append(var4);
+        }
+
+        if (var2 == 0) {
+            var2 = 34;
+        }
+
+        var1.setCharAt(0, (char) var2);
+        var1.append((char) var2);
+        return var1.toString();
+    }
+}
---
+++ Core/src/main/java/org/by1337/bairx/nbt/impl/ByteArrNBT.java
@@ -0,0 +1,65 @@
+package org.by1337.bairx.nbt.impl;
+
+import org.by1337.bairx.nbt.NBT;
+import org.by1337.bairx.nbt.NbtType;
+
+import java.util.Arrays;
+
+public class ByteArrNBT extends NBT {
+    private final byte[] value;
+
+    public ByteArrNBT(byte[] value) {
+        this.value = value;
+    }
+
+    public byte[] getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return arrToString();
+    }
+
+    public String arrToString() {
+        StringBuilder sb = new StringBuilder("[B;");
+        for (Byte b : value) {
+            sb.append(b).append("B,");
+        }
+        sb.setLength(sb.length() - 1);
+        return sb.append("]").toString();
+    }
+
+    @Override
+    public Object getAsObject() {
+        return value;
+    }
+    @Override
+    public NbtType getType() {
+        return NbtType.BYTE_ARR;
+    }
+
+    @Override
+    public String toStringBeautifier(int lvl) {
+        String space = " ".repeat(lvl + 4);
+        StringBuilder sb = new StringBuilder("[ B;\n");
+        for (Byte b : value) {
+            sb.append(space).append(b).append("B,\n");
+        }
+        sb.setLength(sb.length() - 2);
+        return sb.append("\n").append(" ".repeat(lvl)).append("]").toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ByteArrNBT that = (ByteArrNBT) o;
+        return Arrays.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(value);
+    }
+}
